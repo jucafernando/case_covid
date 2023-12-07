7d520d38ebafc91d606a956896dde7b9e01e7c20
Fernando Juca <fernando.juca@outlook.com>
2023-12-07 20:25:28 -0300

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,16 @@
+# Function dependencies, for example:
+# package>=version
+
+pandas>=1.2.4
+Unidecode>=1.2.0
+google-auth>=1.29.0
+google-cloud>=0.34.0
+google-cloud-storage>=1.38.0
+google-cloud-bigquery>=2.13.1
+google-cloud-bigquery-storage>=2.4.0
+requests>=2.25.1
+pyarrow>=4.0.0
+datetime
+timedelta
+gcsfs
+pandas-gbq
